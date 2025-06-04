4cb79d8bbdd6c5bbf8c37b42f7b5c27933a53f46
Chirag Rami <chirag.rami@mindinventory.com>
2025-06-04 10:36:34 +0530

    chore: update .gitignore to exclude key files and add new key index file for Passio key management

---
+++ src/key/index.ts
@@ -0,0 +1,7 @@
+import { PASSIO_KEY as passioKey } from '@env';
+
+export const PASSIO_KEY = passioKey
+
+// If it's showing error, then please create `key` folder at root of the example folder
+// and create folder name with `key` and create index.ts file and add your passio key in that file.
+// We don't use env to avoid the setup of env variables in the machine.
